661ac63571c44819093e6e42b8bf8d3beed3f78b
huangshanqi <hsqmobile@gmail.com>
2014-10-25 12:02:01 +0800

    modefy app register

--- galaxy-all/galaxy-front-all/galaxy-front-web/src/main/java/com/galaxy/front/web/rest/user/controller/RegisterController.java
+++ galaxy-all/galaxy-front-all/galaxy-front-web/src/main/java/com/galaxy/front/web/rest/user/controller/RegisterController.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.galaxy.dal.domain.user.User;
@@ -16,6 +17,8 @@ import com.galaxy.front.web.rest.interceptor.IgnoreAuth;
 import com.galaxy.front.web.rest.model.ResultModel;
 import com.galaxy.front.web.utils.Code;
 import com.galaxy.front.web.utils.EmailUtils;
+import com.galaxy.front.web.utils.ParamUtils;
+import com.galaxy.front.web.utils.RegexUtils;
 import com.galaxy.front.web.utils.ResultModelUtils;
 import com.galaxy.service.user.UserService;
 
@@ -31,6 +34,60 @@ public class RegisterController {
 	private UserService userService;
 	@Autowired
 	private EmailUtils emailUtils;
+	
+	
+	@RequestMapping(value = "register", method = RequestMethod.POST,params = {"username","password","email"})
+	public Object registerByEmail(HttpServletRequest request,@RequestParam("username") String username,@RequestParam("password") String password,@RequestParam("email") String email) {
+		ResultModel resultModel = new ResultModel();
+		
+		if (ParamUtils.isNotEmpty(username,password,email)) {
+			if (userService.countUsersByLoginName(username)>0) {
+				//用户名已注册
+				resultModel = ResultModelUtils.getResultModelByCode(Code.USER_NAME_USED);
+				resultModel.setData("用户名已被注册");
+				return resultModel;
+			}
+			if (userService.countUsersByEmail(email)>0) {
+				//邮箱已注册
+				resultModel = ResultModelUtils.getResultModelByCode(Code.EMAIL_USED);
+				resultModel.setData("邮箱已被注册");
+				return resultModel;
+			}
+			if (RegexUtils.checkAll(username, password, email, null)) {
+				User user = new User();
+				user.setLoginName(username);
+				user.setEmail(email);
+				user.setPassword(password);
+				user.setMobile(null);
+				user.setNick(username);
+				user.setCreatedTime(new Date());
+				user.setEmailAuth(true);
+				user.setMobileAuth(false); 
+				user.setHasPic(false);
+				user.setType(UserType.NORMAL); 
+				userService.createUser(user);
+				
+				//邮箱验证
+				
+				
+				resultModel = ResultModelUtils.getResultModelByCode(Code.OK);
+				resultModel.setData("success");
+				return resultModel;
+			}else {
+				resultModel = ResultModelUtils.getResultModelByCode(Code.REGISTER_ERROR);
+				resultModel.setData("参数不符合要求");
+				return resultModel;
+			}
+		}else {
+			resultModel = ResultModelUtils.getResultModelByCode(Code.PARAMS_ERROR);
+        	resultModel.setData(Code.PARAMS_ERROR.name());
+        	return resultModel;
+		}
+		
+		
+		
+	}
+	
 
 	/**
 	 * 客户端邮箱注册表单提交
@@ -40,10 +97,26 @@ public class RegisterController {
 	 * @param password
 	 * @return
 	 */
+	/*
 	@RequestMapping(value = "register", method = RequestMethod.POST)
 	public Object register(HttpServletRequest request,RegisterForm form) {
 		ResultModel resultModel = new ResultModel();
         String loginName=null;
+        String longType = null;//邮箱或者手机号
+        if (form.getUsername() == null) {
+        	resultModel = ResultModelUtils.getResultModelByCode(Code.PARAMS_ERROR);
+        	resultModel.setData("请填入登录名");
+        	return resultModel;
+		}
+        longType = (form.getEmail()== null)?(form.getPhone_num()):(form.getEmail());
+        if (longType == null) {
+        	resultModel = ResultModelUtils.getResultModelByCode(Code.PARAMS_ERROR);
+        	resultModel.setData("请用邮箱或手机号注册");
+        	return resultModel;
+		}
+        if (userService.countUsersByLoginName(loginName)>0 || userService.c) {
+			
+		}
         if(form.getEmail()!=null){
         	loginName=form.getEmail();
         }
@@ -90,6 +163,7 @@ public class RegisterController {
 		return resultModel;
 		 
 	}
+	*/
 	private void notifyRegister(User user){
 //		String code = MD5Utils.encode2String(email);
 //
