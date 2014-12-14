e71d566fb3c3f055bcd3c9bcfc8fddcef2ba4a12
huangshanqi <hsqmobile@gmail.com>
2014-12-14 11:24:38 +0800

    update

--- galaxy-all/galaxy-front-all/galaxy-front-web/src/main/java/com/galaxy/front/web/rest/model/friend/ContactsItem.java
+++ galaxy-all/galaxy-front-all/galaxy-front-web/src/main/java/com/galaxy/front/web/rest/model/friend/ContactsItem.java
@@ -1,18 +1,18 @@
-package com.galaxy.front.web.rest.model.user;
+package com.galaxy.front.web.rest.model.friend;
 
 import java.io.Serializable;
 
 /*author:huangshanqi
- *time  :2014年9月24日 下午11:04:24
+ *time  :2014年12月14日 上午11:11:09
  *email :hsqmobile@gmail.com
  */
-public class FollowshipUserModel implements Serializable {
-
-	private long userid;// 用户id，8未
+public class ContactsItem implements Serializable {
+	
+	private long userid;// 用户id
 	private String avatar;// 用户头像
 	private String name;// 用户名字
 	private String gender;// 用户性别，male和female
-	private boolean addCard;
+	private boolean addCard;//是否添加了名片
 	public long getUserid() {
 		return userid;
 	}
@@ -44,5 +44,5 @@ public class FollowshipUserModel implements Serializable {
 		this.addCard = addCard;
 	}
 	
-	
+
 }
